60401ffd850ecabab6fbe04cd4c6ffdc91bb12fe
Ben <benoit.gilloz@gmail.com>
2015-02-20 07:09:24 +0100

    First draft of grunt file and refactor of the yeoman generator
    Does not work just yet

--- README.md
+++ README.md
@@ -3,38 +3,4 @@
 A very simple [Yeoman](http://yeoman.io) generator for WordPress starter theme [_s](github.com/automattic/_s).
 
 
-## Getting Started
-
-Install [Yeoman](http://yeoman.io)
-
-```
-npm install -g yo
-```
-
-Install generator-wp-underscores
-
-```
-npm install -g generator-wp-underscores
-```
-
-Create a folder in your WordPress themes folder and initiate the generator
-
-```
-mkdir theme-name && cd $_
-yo wp-underscores
-```
-
-Answer some questions in the prompt and you're done!
-
-Run
-
-```
-grunt
-```
-to watch `.scss` files and live reload!
-
-**Note:** live reload works only if you're working on localhost, 192.168.50.4, or an URL ending in .dev
-
-## License
-
-[MIT License](http://en.wikipedia.org/wiki/MIT_License)
+## 
--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,33 +1,102 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    compass: {
-      dist: {
-        options: {
-          sassDir: 'css/sass',
-          cssDir: 'css',
-          imagesDir: 'images',
-          javascriptsDir: 'js',
-          fontsDir: 'fonts',
-          outputStyle: 'compact',
-          relativeAssets: true,
-          noLineComments: true
+module.exports = function ( grunt )
+{
+  grunt.initConfig( {
+    pkg : grunt.file.readJSON( 'package.json' ),
+
+    // compile your sass
+    sass : {
+      dev : {
+        options : {
+          style : 'expanded'
+        },
+        src : ['app/app.scss'],
+        dest : 'app/css/app.css'
+      },
+      prod : {
+        options : {
+          style : 'compressed'
+        },
+        src : ['app/app.scss'],
+        dest : 'dist/css/app.css'
+      }
+    },
+
+    // watch for changes
+    watch : {
+      scss : {
+        files : ['app/app.scss'],
+        tasks : [
+          'sass:dev',
+          'notify:scss'
+        ],
+        options : {
+          livereload : true
         }
       }
     },
-    watch: {
-      options: {
-        livereload: true
+    // notify cross-OS
+    notify : {
+      scss : {
+        options : {
+          title : 'Grunt, grunt!',
+          message : 'SCSS is all gravy'
+        }
       },
-      compass: {
-        files: ['css/sass/*.scss'],
-        tasks: ['compass']
+      js : {
+        options : {
+          title : 'Grunt, grunt!',
+          message : 'JS is all good'
+        }
+      }
+    },
+
+    requirejs : {
+      compile : {
+        options : {
+          name : '../bootstrap',
+          mainConfigFile : 'app/bootstrap.js',
+          out : 'dist/<%= pkg.name %>.js',
+          include : ['../bower_components/almond/almond.js']
+        }
+      }
+    },
+
+    copy : {
+      main : {
+        files : [
+          // includes files within path
+          {
+            expand : true,
+            src : [ '**', '!bower_components/**', '!js/**', '!*.js', '!*.scss'],
+            cwd : 'app/',
+            dest : 'dist/',
+            filter : 'isFile'}
+        ]
+      }
+    },
+
+    // Show the correct script tag in the dist index.html instead of the requirejs one
+    toggle : {
+      dist : {
+        options : {
+          show : ['dist']
+        },
+        files : {
+          'dist/index.html' : ['dist/index.html']
+        }
+      }
+    },
+    clean : {
+      dist : {
+        dot : false,
+        src : ['dist/*']
       }
     }
-  });
 
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  } );
 
-  grunt.registerTask('default', ['compass', 'watch']);
+  // Load NPM's via matchdep
+  require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+
+  grunt.registerTask( 'build', ['clean:dist', 'copy', 'sass:prod', 'requirejs:compile', 'toggle:dist'] );
 };
