0e1b82e608fd1ad3431a3969e1c97cd430fd3eba
Ben <benoit.gilloz@gmail.com>
2015-02-22 09:16:06 +0100

    First working version

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 'use strict';
-var util = require('util');
-var path = require('path');
-var generators = require('yeoman-generator');
-var fs = require('fs');
-var chalk = require('chalk');
+var util = require('util'),
+    diveSync = require('diveSync'),
+    generators = require('yeoman-generator'),
+    path = require('path'),
+    fs = require('fs'),
+    chalk = require('chalk'),
+    rimraf = require('rimraf');
 
 
 module.exports = generators.Base.extend({
@@ -12,7 +14,7 @@ module.exports = generators.Base.extend({
     // Calling the super constructor is important so our generator is correctly set up
     generators.Base.apply(this, arguments);
   },
-  initializing : function (){
+  initializing : {
 
   },
   prompting : function () {
@@ -61,93 +63,103 @@ module.exports = generators.Base.extend({
       done();
     }.bind(this));
   },
-  default : function (){
-    this.themeroot = this._.slugify(this.themename);
-    //this.mkdir(this.themeroot);
+  default : function () {
+    this.themeId = this._.slugify('My Theme');
   },
-  writing : function (){
-    this.log.info('Downloading & extracting ' + chalk.yellow('_s'));
+  writing : {
+    download : function (){
+      var done = this.async();
 
-    this.remote('Automattic', '_s', this._baseThemeReady.bind(this));
-  },
+      this.mkdir(this.themeId);
+      this.themePath = this.destinationPath(this.themeId);
 
-  _baseThemeReady : function (err, remote){
-    this.mkdir(this.themeroot);
+      this.log.info('Downloading & extracting ' + chalk.yellow('_s'));
 
-    remote.directory('.', this.themeroot);
+      this.extract('https://github.com/Automattic/_s/archive/master.zip', this.themePath, function (){
+        this.workingDirectory = this.themePath + '/_s-master';
+        done();
+      }.bind(this));
+    },
+    stampName : function (){
+      diveSync(this.workingDirectory, function(err, file) {
+        if (err) throw err;
 
-    this._findandreplace(this.themeroot);
-    this.log.ok('Done replacing string ' + chalk.yellow('_s'));
+        this._findAndReplace(file);
+      }.bind(this));
+      
+      this.log.ok('Done replacing string ' + chalk.yellow('_s'));
+    },
+    baseThemeReady : function (){
 
-    this.log(chalk.yellow('Creating dev folders and files'));
-    
-    //this.mkdir(this.themeroot + '/build');
+      var done = this.async();
 
-    this.copy('_main.scss', this.themeroot + '/css/sass/main.scss');
-    this.copy('_theme.js', this.themeroot + '/js/theme.js');
-    this.copy('_package.json', this.themeroot + '/build/package.json');
-    this.copy('_bower.json', this.themeroot + '/build/bower.json');
-    this.copy('Gruntfile.js', this.themeroot + '/build/Gruntfile.js');
-    this.copy('_gitignore', this.themeroot + '/.gitignore');
+      this.log(chalk.yellow('Creating dev folders and files'));
 
-    if (this.sassBootstrap) {
-      this.bowerInstall([ 'sass-bootstrap' ], { save: true });
+      this.copy('_main.scss', this.themePath + '/css/sass/main.scss');
+      this.copy('_theme.js', this.themePath + '/js/theme.js');
+      this.copy('_package.json', this.themePath + '/build/package.json');
+      this.copy('_bower.json', this.themePath + '/build/bower.json');
+      this.copy('Gruntfile.js', this.themePath + '/build/Gruntfile.js');
+      this.copy('_gitignore', this.themePath + '/.gitignore');
+
+      if (this.sassBootstrap) {
+        this.bowerInstall([ 'sass-bootstrap' ], { save: true });
+      }
+
+      this.directory(this.workingDirectory, this.themePath);
+      
+      rimraf(this.workingDirectory, done);
     }
   },
-
-  _findandreplace : function(dir) {
+  _findAndReplace : function (file){
+    
     var self = this,
         _ = this._,
-        files = fs.readdirSync(dir);
+        stat = fs.statSync(file);
 
-    files.forEach(function (file) {
-      file = path.join(dir, file);
-      var stat = fs.statSync(file);
+    if (stat.isFile() && (path.extname(file) == '.php' || path.extname(file) == '.css')) {
+      self.log.info('Find and replace _s in ' + chalk.yellow(file));
+      var data = fs.readFileSync(file, 'utf8');
+      var result;
+      result = data.replace(/Text Domain: _s/g, "Text Domain: " + _.slugify(self.themename) + "");
+      result = result.replace(/'_s'/g, "'" + _.slugify(self.themename) + "'");
+      result = result.replace(/_s_/g, _.underscored(_.slugify(self.themename)) + "_");
+      result = result.replace(/ _s/g, " " + self.themename);
+      result = result.replace(/_s-/g, _.slugify(self.themename) + "-");
 
-      if (stat.isFile() && (path.extname(file) == '.php' || path.extname(file) == '.css')) {
-        self.log.info('Find and replace _s in ' + chalk.yellow(file));
-        var data = fs.readFileSync(file, 'utf8');
-        var result;
-        result = data.replace(/Text Domain: _s/g, "Text Domain: " + _.slugify(self.themename) + "");
-        result = result.replace(/'_s'/g, "'" + _.slugify(self.themename) + "'");
-        result = result.replace(/_s_/g, _.underscored(_.slugify(self.themename)) + "_");
-        result = result.replace(/ _s/g, " " + self.themename);
-        result = result.replace(/_s-/g, _.slugify(self.themename) + "-");
-        if (file == 'style.css') {
-          self.log.info('Updating theme information in ' + file);
-          result = result.replace(/(Theme Name: )(.+)/g, '$1' + self.themename);
-          result = result.replace(/(Theme URI: )(.+)/g, '$1' + self.themeuri);
-          result = result.replace(/(Author: )(.+)/g, '$1' + self.author);
-          result = result.replace(/(Author URI: )(.+)/g, '$1' + self.authoruri);
-          result = result.replace(/(Description: )(.+)/g, '$1' + self.themedescription);
-          result = result.replace(/(Version: )(.+)/g, '$10.0.1');
-          result = result.replace(/(\*\/\n)/, '$1@import url("css/main.css");');
-        }
-        else if (file == 'footer.php') {
-          self.log.info('Updating theme information in ' + file);
-          result = result.replace(/http:\/\/automattic.com\//g, self.authoruri);
-          result = result.replace(/Automattic/g, self.author);
-        }
-        else if (file == 'functions.php') {
-          self.log.info('Updating theme information in ' + file);
-          var themejs = "$1  wp_enqueue_script( '" + _.slugify(self.themename) + "-theme', get_template_directory_uri() . '/js/theme.js', array('jquery'), '0.0.1' );\n  if (in_array($_SERVER['SERVER_ADDR'], ['127.0.0.1', '192.168.50.4']) || pathinfo($_SERVER['SERVER_NAME'], PATHINFO_EXTENSION) == 'dev') {\n    wp_enqueue_script( 'livereload', '//localhost:35729/livereload.js', '', false, true );\n  }\n $2"
-          result = result.replace(/(get_stylesheet_uri\(\) \);\n)(\n.wp_enqueue_script\()/, themejs);
-        }
-        fs.writeFileSync(file, result, 'utf8');
+      if (file == 'style.css') {
+        self.log.info('Updating theme information in ' + file);
+        result = result.replace(/(Theme Name: )(.+)/g, '$1' + self.themename);
+        result = result.replace(/(Theme URI: )(.+)/g, '$1' + self.themeuri);
+        result = result.replace(/(Author: )(.+)/g, '$1' + self.author);
+        result = result.replace(/(Author URI: )(.+)/g, '$1' + self.authoruri);
+        result = result.replace(/(Description: )(.+)/g, '$1' + self.themedescription);
+        result = result.replace(/(Version: )(.+)/g, '$10.0.1');
+        result = result.replace(/(\*\/\n)/, '$1@import url("css/main.css");');
       }
-      else if (stat.isFile() && path.basename(file) == '_s.pot') {
-        self.log.info('Renaming language file ' + chalk.yellow(file));
-        fs.renameSync(file, path.join(path.dirname(file), _.slugify(self.themename) + '.pot'));
+      else if (file == 'footer.php') {
+        self.log.info('Updating theme information in ' + file);
+        result = result.replace(/http:\/\/automattic.com\//g, self.authoruri);
+        result = result.replace(/Automattic/g, self.author);
       }
-      else if (stat.isFile() && path.basename(file) == 'README.md') {
-        self.log.info('Updating ' + chalk.yellow(file));
-        var data = fs.readFileSync(file, 'utf8');
-        var result = data.replace(/((.|\n)*)Getting Started(.|\n)*/i, '$1');
-        fs.writeFileSync(file, result, 'utf8');
+      else if (file == 'functions.php') {
+        self.log.info('Updating theme information in ' + file);
+        var themejs = "$1 wp_enqueue_script( '" + _.slugify(self.themename) + "-theme', get_template_directory_uri() . '/js/theme.js', array('jquery'), '0.0.1' );\n if (in_array($_SERVER['SERVER_ADDR'], ['127.0.0.1', '192.168.50.4']) || pathinfo($_SERVER['SERVER_NAME'], PATHINFO_EXTENSION) == 'dev') {\n wp_enqueue_script( 'livereload', '//localhost:35729/livereload.js', '', false, true );\n }\n $2"
+        result = result.replace(/(get_stylesheet_uri\(\) \);\n)(\n.wp_enqueue_script\()/, themejs);
       }
-      else if (stat.isDirectory()) {
-        self._findandreplace.call(self, file);
-      }
-    });
+
+      fs.writeFileSync(file, result, 'utf8');
+
+    }
+    else if (stat.isFile() && path.basename(file) == '_s.pot') {
+      self.log.info('Renaming language file ' + chalk.yellow(file));
+      fs.renameSync(file, path.join(path.dirname(file), _.slugify(self.themename) + '.pot'));
+    }
+    else if (stat.isFile() && path.basename(file) == 'README.md') {
+      self.log.info('Updating ' + chalk.yellow(file));
+      var data = fs.readFileSync(file, 'utf8');
+      var result = data.replace(/((.|\n)*)Getting Started(.|\n)*/i, '$1');
+      fs.writeFileSync(file, result, 'utf8');
+    }
   }
 });
